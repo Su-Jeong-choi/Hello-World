eeb827646740de4cc74ed7237a3c03efb1d21e03
choi_Su_Jeong <kitchonbear@gmail.com>
2023-10-24 22:44:05 +0900

    first commit!!

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // i(n put)o(ut put)stream(=콘솔창에 입력 또는 출력) 이미 작성된 (헤더)문서를 가져옴
+
+using namespace std; // std라는 이름공간을 사용하겠다 예시) std::cout(표준출력스트림) 처럼 매번 써야하는 수고스럼을 덜기 위해
+
+int main() // 메인이라는 이름을 가진 int 함수다. 괄호로 감싸줘야함! entry point=시작 지점
+{
+
+	cout << "Hello World" << endl;	
+//	printf("Hello World!!! by printf"); -> C언어에서 씀 (참고용)
+/* cout->c(haracter = 문자)out(출력하다) : 문자뿐만 아니라 부동소수점 정수도 가능!
+ << (시냇물처럼 흘려 보내서 화면에 출력해 준다(스트림인설트오퍼레이터=입력_스트림_연산자) << endl(ine=끝선); -> 명령문 끝에는 항상 *세마이클론이 존재함!*/
+
+
+	return 0; // return=반환 해 준다 int(인티저) 정수형의 값0을 반환 해준다 = 운영체제에게 잘 끝났다라는 뜻! (알려주는 역할)
+
+}
